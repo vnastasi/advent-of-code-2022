c0613c8a6f8cd99b5b4de7b0d2c6445586ed5295
Valentin Nastasi <pragmatiku@protonmail.com>
2023-01-02 17:35:05 +0200

    Day 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.jetbrains:annotations:23.1.0")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("--enable-preview")
 }
